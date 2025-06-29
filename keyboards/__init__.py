014d1b7ef9a7ab445e8395e58dfd05c215d1dd09
Alexander Kovzan <alexanderkovzan@gmail.com>
2025-06-29 08:53:09 +0300

    qwe

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -1,3 +1,4 @@
 from .set_main_menu import set_main_menu
 from .pagination_keyboard import build_pagination_kb
 from .bookmarks_keyboard import build_bookmarks_keyboard, build_edit_bookmarks_kb
+#adfsdf
